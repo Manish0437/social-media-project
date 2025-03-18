f0051e34a78e6bdae2d6114f7826bb2378867885
mallolumanishbabu <mallolumanishbabu@gmail.com>
2025-03-18 12:34:42 +0530

    first-commit

--- src/context/ProfileDetailsContext.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-
-// const ProfileDetailsContext=React.createContext({
-//     profileData:{},
-//     changeProfileData:() => {},
-// })
-
-// export default ProfileDetailsContext
